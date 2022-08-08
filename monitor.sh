8dd5ddc8fe2a65e3822f0ee67e88370e2f48f11c
nicolotellini <nicolo.tellini@unice.fr>
2022-08-08 18:58:18 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -6,6 +6,9 @@ pipeline=mypipeline # the name of your pipeline. It is used to send you an appor
 soft1=bwa-mem2 # the name of the software we want to check the link status.
 soft2=samtools  # the name of the software we want to check the link status.
 
+user1=
+user2=
+
 # CHANGE THE PATH/TO/telegram-send at line 42
 
 cd $rundir
@@ -15,7 +18,7 @@ mkdir $pipeline
 cd $pipeline
 
 # link 1
-/usr/bin/time -v git clone --recursive https://github.com/$soft1/$soft1.git 2> $soft1.err
+/usr/bin/time -v git clone --recursive https://github.com/$user1/$soft1.git 2> $soft1.err
 
 check=$(grep Exit $soft1.err | cut -d":" -f2 | tr -d "[:blank:]")
 	if [ $check != 0 ]
@@ -26,7 +29,7 @@ check=$(grep Exit $soft1.err | cut -d":" -f2 | tr -d "[:blank:]")
 	fi
 
 # link 2
-/usr/bin/time -v git clone --recursive https://github.com/$soft2/$soft2.git 2> $soft2.err
+/usr/bin/time -v git clone --recursive https://github.com/$user2/$soft2.git 2> $soft2.err
 
 check=$(grep Exit $soft2.err | cut -d":" -f2 | tr -d "[:blank:]")
 	if [ $check !=  0 ]
