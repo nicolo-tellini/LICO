a4d2e03f855ebfea8e3ee9d16b7c645892f99a4d
nicolotellini <nicolo.tellini@unice.fr>
2022-08-08 21:46:38 +0200

    Update monitor2.sh

--- monitor2.sh
+++ monitor2.sh
@@ -15,41 +15,33 @@ cd $pipeline
 time=$(date)
 
 controller () {
-  exfile=$1
-  soft=$2
-  con=$(grep -w Exit $exfile | cut -d":" -f2 | tr -d "[:blank:]")
+ exfile=$1
+ soft=$2
+ con=$(grep -w Exit $exfile | cut -d":" -f2 | tr -d "[:blank:]")
   if [ $con != 0 ]
   then
-    var_sms="$var_sms, $soft:Exit"
-   else
-    var_sms="$var_sms, $soft:OK"
-   fi
-}
+  var_sms="$var_sms, $soft:Exit"
+  else
+  var_sms="$var_sms, $soft:OK"
+  fi
+  }
 
 var_sms=""
 while read -r link;
 do
-
-githubrepo=$(grep "github.com" $line)
-
-if [ -z "$githubrepo" ]
-then
-
-# HTTP link
-soft=$(echo $link | rev | cut -d"/" -f1 | rev)
-/usr/bin/time -v wget $link 2> $soft.err
-
-controller $soft.err $soft
-
-else
-
-# GitHub link
-soft=$(echo $link | rev | cut -d"/" -f1 | rev | cut -d"." -f1)
-/usr/bin/time -v git clone --recursive $link 2> $soft.err
-
-controller $soft.err $soft
-fi
-
+ githubrepo=$(grep "github.com" $line)
+  if [ -z "$githubrepo" ]
+  then
+  # HTTP link
+  soft=$(echo $link | rev | cut -d"/" -f1 | rev)
+  /usr/bin/time -v wget $link 2> $soft.err
+  controller $soft.err $soft
+  else
+  # GitHub link
+  soft=$(echo $link | rev | cut -d"/" -f1 | rev | cut -d"." -f1)
+  /usr/bin/time -v git clone --recursive $link 2> $soft.err
+  controller $soft.err $soft
+  fi
 done < $file
 
 telegram-send "$time" "LICO report" "Pipeline $pipeline:" "$var_sms"
