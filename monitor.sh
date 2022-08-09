2d5dd091321c9233f2ebee347822d91cc4e9c367
nicolotellini <nicolo.tellini@unice.fr>
2022-08-09 17:11:24 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,7 +3,15 @@
 while getopts "l:p:n:h" arg; do
   case $arg in
     h) echo "Usage:"
-       echo "monitor.sh -l file.txt -p /path/to/dir/where/file.txt/is/stored -n pipeline name"
+       echo ""
+       echo "monitor.sh -l <file.txt> -p <rundir> -n <pipeline>"
+       echo ""
+       echo "<file.txt>: a txt file with the GitHub/HTTP links (one link per line);"
+       echo ""
+       echo "<rundir>: path to /favorite/dir where <file.txt> is stored;"
+       echo ""
+       echo "<pipeline>: the name of your pipeline (to send you a meaningful report)."
+       echo ""
        exit 0
        ;;
     n) pipeline=$OPTARG;;
